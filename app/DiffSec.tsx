2b9bfd40ab4b2f1361c61cbb0e475f5127b9e288
omarJerbi <omarjerbi98@gmail.com>
2023-04-19 14:43:13 +0200

    try fix favicon

--- app/DiffSec.tsx
+++ app/DiffSec.tsx
@@ -9,7 +9,7 @@ interface props {
 
 export function DiffSec({ texts, color, tit }: props) {
     return <div id='sizereact' className={'diffsec h-2/5 bg-slate-900 flex flex-col w-4/6 px-4 py-4 rounded-md border border-gray-800 overflow-y-auto max-sm:w-full max-sm:h-5/6'}>
-        <div className={(color == 'w' ? "text-white" : "text-green-300") + " text-3xl text-center mb-3 font-bold tracking-wider " + tit} style={{ textShadow: `white 1px 1px 2px;` }}>
+        <div className={(color == 'w' ? "text-white" : "text-green-300") + " text-3xl text-center mb-3 font-bold tracking-wider " + tit} style={{ textShadow: `white 1px 1px 2px` }}>
             {texts[0]}
         </div>
         {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import Navbar from './components/Navbar/Navbar'
 import { UserContextProvider } from './contexts/userContext'
 import './globals.css'
+import i from './favicon.png'
+import Image from 'next/image'
+
 
 export const metadata = {
-  title: 'OJXacademy.com',
+  title: "OJXacademy | Impara l'arte dello sviluppo web!",
   description: 'Con OJXacademy imparerai le pricipali tecnologie e tecniche di programmazione utilizzate per lo sviluppo web: Con corsi sia in gruppo che individuali, inizierai in poco tempo a creare i tuoi primi siti web per desktop e mobile!',
   icons: {
-    icon: "/icon.png"
+    icon: "/_next/static/media/favicon.1d647803.png"
   },
   keywords: ["ojxacademy", 'sviluppo web', "corso sviluppo web", "javascript", "programmazione", "html", "css", "react"],
-  authors: [{name: "OJ", url: "https://github.com/Omar-jerbi"}]  ,
+  authors: [{ name: "OmarJ", url: "https://github.com/Omar-jerbi" }],
   category: "sviluppo web"
 }
 
@@ -18,10 +21,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  
+
   return (
     <html lang="en">
       <body className='text-white'>
+        {/* FIX DEL CAZZO per forzare build la icon in static folder */}
+        <Image src={i} alt='sss' className='hidden' height={0} width={0} />
 
         <UserContextProvider>
           <Navbar />
